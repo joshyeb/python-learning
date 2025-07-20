24d9561d89131b073e92994558e8d2fe0c1044f5
Josh <joshuayeboah456@gmail.com>
2025-07-20 21:43:51 +0000

    Add files via upload

---
+++ Practice.py
@@ -0,0 +1,12 @@
+#Name of the user
+name = input("What is your name? ")
+
+# Index number of the user
+indexNumber= input("What is your index number? ")
+
+# Cumulative Weighted Average (CWA) of the user
+cwa = input("What is your CWA? ")
+
+# Amount spent by the user in a day
+amount = float(input("What is the amount you spend in a day? "))
+print(f"Hello, {name}! Your index number is {indexNumber}, your CWA is {cwa}, and you spend {amount:.2f} in a day.\n You're a good student keep it up!")
