1441c666df563a659a074534a36ab0824451d5ae
Josh <joshuayeboah456@gmail.com>
2025-07-20 23:54:40 +0000

    Add files via upload

---
+++ whileLoop.py
@@ -0,0 +1,17 @@
+#while(CONDITION IS TRUE)
+    #carry out some statement
+
+
+#import random
+#number = random.randint(1,10)
+
+
+count = 10
+
+while (count > 0):
+    print(count)
+    count -= 1    #count = count - 1
+    if (count == 5):
+        continue
+    print(count)
+        
