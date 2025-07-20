2b70d623a5225abe302c48f63fb9ee8216ffb309
Josh <joshuayeboah456@gmail.com>
2025-07-20 22:21:27 +0000

    Add files via upload

---
+++ nextedLoop.py
@@ -0,0 +1,10 @@
+#for iterator in range(start,stop,step)
+#   some statement to execute
+
+
+
+#This loop will make the loop run from j(1,6) 10 times because of i(1,11)
+for i in range(1,11):
+    print(f"Multiples of {i}:")
+    for j in range(0,28,2):
+        print(f"\t{j}")
