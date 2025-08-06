9b25f00273bb22118bd870de2d0df979a32845a1
Josh <joshuayeboah456@gmail.com>
2025-08-06 11:50:54 +0000

    Add files via upload

---
+++ function.py
@@ -0,0 +1,10 @@
+#built in functins eg.print,type,input
+#funtion definiton - explaining what the function does
+#function initialization
+#global function - you can call that function anywhere
+# def - tells the compiler that youare coming to aadd a function
+def add (input1 , input2):
+    return input1 + input2
+
+age = add (2,3)
+print("your age is ", age)
